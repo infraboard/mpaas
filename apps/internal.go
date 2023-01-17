ea1837d95f870524ddcce2bc707b0c46ffdb62d6
yumaojun03 <18108053819@163.com>
2023-01-17 10:22:31 +0800

    补充健康检查模块

--- apps/internal.go
+++ apps/internal.go
@@ -1,6 +1,9 @@
 package apps
 
 import (
+	// 监控检查实现
+	_ "github.com/infraboard/mpaas/apps/health/impl"
+
 	// 注册所有GRPC服务模块, 暴露给框架GRPC服务器加载, 注意 导入有先后顺序
 	_ "github.com/infraboard/mpaas/apps/cluster/impl"
 	_ "github.com/infraboard/mpaas/apps/deploy/impl"
