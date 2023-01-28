6c94a27afb55ccf2d5516dafb7721f37a902161d
yumaojun03 <18108053819@163.com>
2023-01-28 09:54:28 +0800

    补充readme说明

--- apps/http.go
+++ apps/http.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/infraboard/mcube/app/health/api"
 
 	// 注册所有HTTP服务模块, 暴露给框架HTTP服务器加载
+	_ "github.com/infraboard/mpaas/apps/build/api"
 	_ "github.com/infraboard/mpaas/apps/cluster/api"
 	_ "github.com/infraboard/mpaas/apps/deploy/api"
 	_ "github.com/infraboard/mpaas/apps/gateway/api"
--- apps/internal.go
+++ apps/internal.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/infraboard/mpaas/apps/health/impl"
 
 	// 注册所有GRPC服务模块, 暴露给框架GRPC服务器加载, 注意 导入有先后顺序
+	_ "github.com/infraboard/mpaas/apps/build/impl"
 	_ "github.com/infraboard/mpaas/apps/cluster/impl"
 	_ "github.com/infraboard/mpaas/apps/deploy/impl"
 	_ "github.com/infraboard/mpaas/apps/gateway/impl"
