0ffc097a80c0344d1824e37d70a98a0c96ae2c30
yumaojun03 <18108053819@163.com>
2023-01-30 14:04:16 +0800

    补充task runner

---
+++ apps/task/runner/interface.go
@@ -0,0 +1,14 @@
+package runner
+
+import (
+	"context"
+
+	"github.com/infraboard/mpaas/apps/task"
+)
+
+type Runner interface {
+	Run(context.Context, *RunRequest) (*task.Task, error)
+}
+
+type RunRequest struct {
+}
