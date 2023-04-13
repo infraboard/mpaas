b8426c6caac676473a2a0f48f990d857b5021038
yumaojun03 <18108053819@163.com>
2023-04-13 09:55:30 +0800

    制品管理

--- apps/artifact/artifact_enum.pb.go
+++ apps/artifact/artifact_enum.pb.go
@@ -54,24 +54,24 @@ func (t *TYPE) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-// ParsePLATFORMFromString Parse PLATFORM from string
-func ParsePLATFORMFromString(str string) (PLATFORM, error) {
+// ParseARCHFromString Parse ARCH from string
+func ParseARCHFromString(str string) (ARCH, error) {
 	key := strings.Trim(string(str), `"`)
-	v, ok := PLATFORM_value[strings.ToUpper(key)]
+	v, ok := ARCH_value[strings.ToUpper(key)]
 	if !ok {
-		return 0, fmt.Errorf("unknown PLATFORM: %s", str)
+		return 0, fmt.Errorf("unknown ARCH: %s", str)
 	}
 
-	return PLATFORM(v), nil
+	return ARCH(v), nil
 }
 
 // Equal type compare
-func (t PLATFORM) Equal(target PLATFORM) bool {
+func (t ARCH) Equal(target ARCH) bool {
 	return t == target
 }
 
 // IsIn todo
-func (t PLATFORM) IsIn(targets ...PLATFORM) bool {
+func (t ARCH) IsIn(targets ...ARCH) bool {
 	for _, target := range targets {
 		if t.Equal(target) {
 			return true
@@ -82,7 +82,7 @@ func (t PLATFORM) IsIn(targets ...PLATFORM) bool {
 }
 
 // MarshalJSON todo
-func (t PLATFORM) MarshalJSON() ([]byte, error) {
+func (t ARCH) MarshalJSON() ([]byte, error) {
 	b := bytes.NewBufferString(`"`)
 	b.WriteString(strings.ToUpper(t.String()))
 	b.WriteString(`"`)
@@ -90,8 +90,53 @@ func (t PLATFORM) MarshalJSON() ([]byte, error) {
 }
 
 // UnmarshalJSON todo
-func (t *PLATFORM) UnmarshalJSON(b []byte) error {
-	ins, err := ParsePLATFORMFromString(string(b))
+func (t *ARCH) UnmarshalJSON(b []byte) error {
+	ins, err := ParseARCHFromString(string(b))
+	if err != nil {
+		return err
+	}
+	*t = ins
+	return nil
+}
+
+// ParseOSFromString Parse OS from string
+func ParseOSFromString(str string) (OS, error) {
+	key := strings.Trim(string(str), `"`)
+	v, ok := OS_value[strings.ToUpper(key)]
+	if !ok {
+		return 0, fmt.Errorf("unknown OS: %s", str)
+	}
+
+	return OS(v), nil
+}
+
+// Equal type compare
+func (t OS) Equal(target OS) bool {
+	return t == target
+}
+
+// IsIn todo
+func (t OS) IsIn(targets ...OS) bool {
+	for _, target := range targets {
+		if t.Equal(target) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// MarshalJSON todo
+func (t OS) MarshalJSON() ([]byte, error) {
+	b := bytes.NewBufferString(`"`)
+	b.WriteString(strings.ToUpper(t.String()))
+	b.WriteString(`"`)
+	return b.Bytes(), nil
+}
+
+// UnmarshalJSON todo
+func (t *OS) UnmarshalJSON(b []byte) error {
+	ins, err := ParseOSFromString(string(b))
 	if err != nil {
 		return err
 	}
