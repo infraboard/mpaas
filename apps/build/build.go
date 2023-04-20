f3bca74fd4b47464d3405852a554ef01ae4c6388
yumaojun03 <18108053819@163.com>
2023-04-20 17:15:44 +0800

    补充glob特殊处理

--- apps/build/build.go
+++ apps/build/build.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"path/filepath"
 	"regexp"
+	"strings"
 
 	"github.com/infraboard/mpaas/apps/job"
 	"github.com/infraboard/mpaas/common/meta"
@@ -148,7 +149,8 @@ func (t *Trigger) MatchSubEvent(pattern string) bool {
 	for _, b := range t.SubEvents {
 		switch t.SubEventsMatchType {
 		case MATCH_TYPE_GLOB:
-			ok, _ := filepath.Match(pattern, b)
+			// 匹配之前需要特殊处理, 因为/对于filepath标识为一个目录, 需要替换掉，扁平处理
+			ok, _ := filepath.Match(pattern, strings.ReplaceAll(b, "/", "."))
 			if ok {
 				return true
 			}
