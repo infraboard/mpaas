e0157f3115ca3a5240c078d4da152aa63e3a61ee
yumaojun <18108053819@163.com>
2023-04-29 09:24:41 +0800

    补充审核模版

--- apps/approval/impl/notify/template.go
+++ apps/approval/impl/notify/template.go
@@ -1,14 +1,63 @@
 package notify
 
-import "embed"
+import (
+	"bytes"
+	"embed"
+	"text/template"
+)
 
 //go:embed templates/*
 var templatesDir embed.FS
 
-func FeishuAuditNotifyTemplate() (string, error) {
-	content, err := templatesDir.ReadFile("templates/feishu_card.json")
+func NewFeishuAuditNotifyMessage() *FeishuAuditNotifyMessage {
+	return &FeishuAuditNotifyMessage{
+		ShowPassButton: true,
+		ShowDenyButton: true,
+	}
+}
+
+type FeishuAuditNotifyMessage struct {
+	// 标题
+	Title string
+	// 申请人
+	CreateBy string
+	// 执行人
+	Operator string
+	// 其他审核人
+	Auditor string
+	// 流水线描述
+	PipelineDesc string
+	// 执行方式
+	ExecType string
+	// 执行时变量
+	ExecVars string
+	// 是否显示同意按钮
+	ShowPassButton bool
+	// 同意按钮的名称
+	PassButton string
+	// 是否显示拒绝按钮
+	ShowDenyButton bool
+	// 拒绝按钮的名称
+	DenyButton string
+	// 备注
+	Note string
+}
+
+func (t *FeishuAuditNotifyMessage) Render() (string, error) {
+	content, err := templatesDir.ReadFile("templates/feishu_card.tmpl")
 	if err != nil {
 		return "", err
 	}
-	return string(content), nil
+
+	tmpl, err := template.New("feishu_card").Parse(string(content))
+	if err != nil {
+		return "", err
+	}
+	buf := bytes.NewBufferString("")
+	err = tmpl.Execute(buf, t)
+	if err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
 }
--- apps/approval/impl/notify/template_test.go
+++ apps/approval/impl/notify/template_test.go
@@ -1,1 +1,16 @@
 package notify_test
+
+import (
+	"testing"
+
+	"github.com/infraboard/mpaas/apps/approval/impl/notify"
+)
+
+func TestFeishuAuditNotifyTemplate(t *testing.T) {
+	msg := notify.NewFeishuAuditNotifyMessage()
+	content, err := msg.Render()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(content)
+}
