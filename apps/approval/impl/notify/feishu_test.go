1fcbcfde13efb0ca953cbe057ec0e8bf20b09e7e
yumaojun <18108053819@163.com>
2023-04-29 09:53:10 +0800

    测试审核通知

---
+++ apps/approval/impl/notify/feishu_test.go
@@ -0,0 +1,71 @@
+package notify_test
+
+import (
+	"testing"
+
+	"github.com/infraboard/mcenter/apps/domain"
+	"github.com/infraboard/mcenter/apps/namespace"
+	mcenter_notify "github.com/infraboard/mcenter/apps/notify"
+	"github.com/infraboard/mpaas/apps/approval/impl/notify"
+)
+
+var feishuNotifyCard = &notify.FeishuAuditNotifyMessage{
+	Title:          "xxx提交的「系统帐号及权限申请」待你审批",
+	CreateBy:       "[@王冰](https://open.feishu.cn/document/ugTN1YjL4UTN24CO1UjN/uUzN1YjL1cTN24SN3UjN?from=mcb)",
+	Operator:       "xxxx",
+	Auditor:        "xxx",
+	PipelineDesc:   "xxx流水线",
+	ExecType:       "手动执行",
+	ExecVars:       "\nxx\nxxx\n",
+	ShowPassButton: true,
+	PassButton:     "同意",
+	ShowDenyButton: true,
+	DenyButton:     "拒绝",
+	Note:           "备注信息",
+	ApprovalId:     "xxx",
+}
+
+func TestFeishuCardAuditNotity(t *testing.T) {
+	msg := feishuNotifyCard
+	content, err := msg.Render()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(content)
+}
+
+func TestFeishuCardPassNotify(t *testing.T) {
+	msg := feishuNotifyCard
+	msg.ShowDenyButton = false
+	msg.PassButton = "xxx已同意"
+	content, err := msg.Render()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(content)
+}
+
+func TestFeishuCardDenyNotify(t *testing.T) {
+	msg := feishuNotifyCard
+	msg.ShowPassButton = false
+	msg.DenyButton = "xxx已拒绝"
+	content, err := msg.Render()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(content)
+
+	req := mcenter_notify.NewSendNotifyRequest()
+	req.Domain = domain.DEFAULT_DOMAIN
+	req.Namespace = namespace.DEFAULT_NAMESPACE
+	req.NotifyTye = mcenter_notify.NOTIFY_TYPE_IM
+	req.AddUser("admin")
+	req.Title = msg.Title
+	req.ContentType = "interactive"
+	req.Content = content
+	res, err := nrpc.SendNotify(ctx, req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(res)
+}
--- apps/approval/impl/notify/notify_test.go
+++ apps/approval/impl/notify/notify_test.go
@@ -1,1 +1,20 @@
 package notify_test
+
+import (
+	"context"
+
+	"github.com/infraboard/mpaas/test/tools"
+
+	"github.com/infraboard/mcenter/apps/notify"
+	"github.com/infraboard/mcenter/client/rpc"
+)
+
+var (
+	nrpc notify.RPCClient
+	ctx  = context.Background()
+)
+
+func init() {
+	tools.DevelopmentSetup()
+	nrpc = rpc.C().Notify()
+}
