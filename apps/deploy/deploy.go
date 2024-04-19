ef8050069e97e7eeb02bd88d140a0ff3281da136
yumaojun03 <719118794@qq.com>
2024-04-19 10:09:38 +0800

    先清楚pod后合并

--- apps/deploy/deploy.go
+++ apps/deploy/deploy.go
@@ -138,17 +138,18 @@ func (c *K8STypeConfig) Merge(target *K8STypeConfig) error {
 		return fmt.Errorf("状态更新, 禁止修改集群, 修改值: %s", target.ClusterId)
 	}
 
-	err := mergo.MergeWithOverwrite(c, target)
-	if err != nil {
-		return err
-	}
-
 	// 清除删除的Pod
 	for k, v := range target.Pods {
 		if v == "" {
 			delete(c.Pods, k)
 		}
 	}
+
+	err := mergo.MergeWithOverwrite(c, target)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
